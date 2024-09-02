ecfe083fb9210e571b1af799eef2c430e48f9a59
Gorra.js <gorrahabla1234@gmail.com>
2024-09-02 16:55:11 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+	const [term, setTerm] = useState([]);
+	const [term2, setTerm2] = useState([]);
+	const [search, setSearch] = useState("");
 
-  const [term, setTerm] = useState("")
+	const getWeb = () => {
+		fetch("https://api.adviceslip.com/advice")
+			.then((res) => res.json())
+			.then((data) => setTerm(data.slip.advice));
+	};
 
-  const handleTermChange = (event) => setTerm(event.target.value);
+	const buscar = () => {
+		fetch(`https://api.adviceslip.com/advice/search/${search}`)
+			.then((res) => res.json())
+			.then((data) => setTerm2(data.slips[0].advice));
+	};
 
-  return (
-    <main>
+	const handleTermChange = (e) => setSearch(e.target.value);
 
-      <h1>Evaluación React - Requests</h1>
-      <h1>Consejos de vida</h1>
+	const Submit1 = (e) => {
+		e.preventDefault();
+    setTerm(getWeb());
+	};
 
+  const Submit2 = (e) => {
+    e.preventDefault()
+    setTerm2(buscar())
+  }
+
+	return (
+		<main>
+			<h1>Evaluación React - Requests</h1>
+			<h1>Consejos de vida</h1>
+			<h3>{getWeb()}</h3>
       <div>
-        <h2>Obtener un consejo aleatorio</h2>
-        <button>Obtener</button>
-        <p className="result-box"></p>
-      </div>
+				<h2>Obtener un consejo aleatorio</h2>
+				<button type="submit" onClick={Submit1}>
+					Obtener
+				</button>
+				<p className="result-box">{term}</p>
+			</div>
 
-      <div>
-        <h2>Buscar un consejo</h2>
-        <input type="text" onChange={handleTermChange} />
-        <button>Enviar</button>
-        <h3>Resultados de búsqueda:</h3>
-        <p className="result-box"></p>
-      </div>
-
-    </main>
-  );
+			<div>
+				<h2>Buscar un consejo</h2>
+				<input type="text" value={search} onChange={handleTermChange} />
+				<button type="submit" onClick={Submit2}>Enviar</button>
+				<h3>Resultados de búsqueda:</h3>
+				<p className="result-box">{term2}</p>
+			</div>
+		</main>
+	);
 }
 
 export default App;
